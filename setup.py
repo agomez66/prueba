44befa5a634cd8e7801347f24c6cb8a3e16ec052
agomez66 <58739032+agomez66@users.noreply.github.com>
2020-03-11 16:34:45 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'beans',
+  packages = ['beans'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python Connector for AutomaticApiRest',
+  author = 'Abraham Gómez',
+  author_email = 'agomez66@xtec.cat',
+  url = 'https://github.com/agomez66/prueba', # use the URL to the github repo
+  download_url = 'https://github.com/agomez66/prueba/tarball/0.1', # I'll explain this in a second
+  keywords = ['beans','ejemplo','example'], # arbitrary keywords
+  classifiers = [],
+)
